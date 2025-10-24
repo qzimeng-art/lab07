5fe2313e3d802c17fa1006d8eaab759566851f25
qzimeng-art <qzimeng@ualberta.ca>
2025-10-23 19:21:01 -0600

    Finish Lab07

--- app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
@@ -1,6 +1,7 @@
 package com.example.androiduitesting;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
@@ -18,14 +19,20 @@ import static org.hamcrest.CoreMatchers.anything;
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.hamcrest.CoreMatchers.is;
 
+import android.app.Activity;
+import android.app.Instrumentation;
 import android.content.Intent;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.action.ViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
+import androidx.test.runner.lifecycle.Stage;
 
 import com.example.androiduitesting.MainActivity;
 import com.example.androiduitesting.R;
@@ -34,6 +41,8 @@ import com.example.androiduitesting.ShowActivity;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Collection;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
@@ -52,7 +61,10 @@ public class ShowActivityTest {
 
     @Test
     public void testBackButton() {
+        ActivityScenario<ShowActivity> activityScenario = ActivityScenario.launch(ShowActivity.class);
         onView(withId(R.id.button_back)).perform(click());
+        activityScenario.close();
     }
-
 }
+
+
